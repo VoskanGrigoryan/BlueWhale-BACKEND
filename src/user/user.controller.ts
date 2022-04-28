840ca9d6185414bd968526dc7b20f775ef60ccd7
VoskanGrigoryan <voskan.grigoryan.arg@mail.com>
2022-04-28 00:25:41 -0300

    adding guard for getById route for testing

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, Get, Post, Req, Request } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { UserInterface } from './interfaces/user.interface';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private UserService: UserService) {}
+
+  @UseGuards(JwtGuard)
+  @Get(':id')
+  getUser(@Param('id') id: string): Promise<UserInterface | null> {
+    return this.UserService.findUserById(id);
+  }
 }
